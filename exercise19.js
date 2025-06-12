14c0b442f8eab6bd6c2bf1b40f25d6cb93b5331b
StormieMurphy <stolenmurphy@gmail.com>
2025-06-12 10:50:50 -0500

    Completed OJT Exercise 19

---
+++ exercise19.js
@@ -0,0 +1,17 @@
+function classifyTriangle(sideOne, sideTwo, sideThree){
+    if (sideOne == sideTwo && sideTwo == sideThree){
+        console.log("This triangle is equilateral."); 
+    }
+    else if (sideOne == sideTwo || sideTwo == sideThree || sideThree == sideOne){
+        console.log("This triangle is isosceles.");
+    }
+    else {
+        console.log("This triangle is scalene.");
+        
+    };
+};
+classifyTriangle(10, 10, 10);
+classifyTriangle(15, 20, 15);
+classifyTriangle(15, 15, 20);
+classifyTriangle(20, 15, 15);
+classifyTriangle(25, 34, 42);
